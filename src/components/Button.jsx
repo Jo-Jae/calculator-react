0df1dd66d6653bc5ddfae19e9775fb2e50c15700
Jo-Jae <whwodjs11@naver.com>
2024-05-10 17:17:24 +0900

    color

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,11 @@
 const Button = ({ name, onClickButton }) => {
   return (
     <button
-      className="bg-gray-200 w-10 h-10 rounded-md flex justify-center items-center shadow-sm"
+      className={`bg-gray-200 w-10 h-10  rounded-md flex justify-center items-center shadow-sm 
+      ${name === "+" && "bg-red-500"}
+      ${name === "-" && "bg-orange-500"}
+      ${name === "*" && "bg-yellow-500"}
+      ${name === "/" && "bg-green-500"}`}
       onClick={onClickButton}
     >
       {name}
