44d8e362511438a1dc69f5f2609a64deb96b6d8f
Jo-Jae <whwodjs11@naver.com>
2024-05-10 14:50:08 +0900

    onClickButton - Number

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,18 @@
-const Button = ({ name }) => {
+const Button = ({ name, result, setResult }) => {
+  const onClickButton = () => {
+    setResult(result + name);
+    if (result === "0") {
+      setResult(name);
+    } else {
+      setResult(result + name);
+    }
+  };
+
   return (
-    <button className="bg-gray-200 w-10 h-10 rounded-md flex justify-center items-center shadow-md">
+    <button
+      className="bg-gray-200 w-10 h-10 rounded-md flex justify-center items-center shadow-sm"
+      onClick={onClickButton}
+    >
       {name}
     </button>
   );
