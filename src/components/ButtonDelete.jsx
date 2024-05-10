6a91b39166cae7cb77b0e395ed21914572a20ca0
Jo-Jae <whwodjs11@naver.com>
2024-05-10 16:35:13 +0900

    ButtonEnter

---
+++ src/components/ButtonDelete.jsx
@@ -0,0 +1,9 @@
+import Button from "./Button";
+
+const ButtonDelete = ({ name }) => {
+  const onClickButton = () => {};
+
+  return <Button name={name} onClickButton={onClickButton} />;
+};
+
+export default ButtonDelete;
